c6867dc48b7e32a04a815c35482ee8eecee829c9
pluveto <receding@protonmail.com>
2021-07-23 20:42:20 +0800

    chore: move models

--- pkg/bloggo/model/model.go
+++ pkg/bloggo/model/model.go
@@ -5,3 +5,48 @@ type Setting struct {
 	Value string
 	Type  string
 }
+
+// == USER ==
+
+type User struct {
+	ID          uint64
+	Username    string
+	Password    string
+	Email       string
+	Description string
+	AvatarUrl   string
+}
+
+type Token struct {
+	UserID     uint64
+	TokenValue string
+	ExpiredAt  int64
+}
+
+type UserServicer interface {
+	Login(username string, password string) *Token
+	Logout(tokenValue string)
+	Create(user *User)
+}
+
+// == POST ==
+
+type Post struct {
+	ID          uint64
+	AuthorID    uint64
+	Path        string
+	Slug        string
+	Title       string
+	Content     string
+	PublishedAt int64
+	ModifiedAt  int64
+}
+
+type Comment struct {
+	ID          uint64
+	PostID      uint64
+	Content     string
+	AuthorName  string
+	AuthorEmail string
+	AuthorUrl   string
+}
