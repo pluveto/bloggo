4e08eb38105b139c0ddc018d0d702c0e39edf2c2
pluveto <receding@protonmail.com>
2021-07-23 17:31:16 +0800

    feat: update schema for gorm

--- install/schema.sql
+++ install/schema.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS "sys_site_setting"(
-setting_key text primary key not null,
-setting_value text not null,
-setting_type text not null
+CREATE TABLE IF NOT EXISTS "settings"(
+key text primary key not null,
+value text not null,
+type text not null
 );
